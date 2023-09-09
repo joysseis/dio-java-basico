9def2be04ebaa8f3c3eb89b3dc15a254f6ed2120
Joyce Silva <joyce.silva@tucurui.ufpa.br>
2023-09-08 23:29:51 -0300

    deafio contador mais exceções

---
+++ DesafioControleFluxo/DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+
+public class ParametrosInvalidosException extends RuntimeException {
+
+    public ParametrosInvalidosException(){
+    System.out.println("o segundo parametro deve ser maior que o primeiro");
+    }
+}
